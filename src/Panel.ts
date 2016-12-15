adb34a2af9341563358e80bf86797c3cc2b5b3eb
Brad Sickles <brad.sickles@gmail.com>
2016-12-15 09:31:59 -0500

    Creating StackPanel. (#2)
    * Creating Panel.
    * Creating StackPanel.
    * Providing measureOverride and arrangeOverride by default.

---
+++ src/Panel.ts
@@ -0,0 +1,111 @@
+/// <reference path="core/LayoutNode" />
+
+namespace mirage {
+    export class Panel extends core.LayoutNode {
+        tree: IPanelTree;
+
+        protected createTree(): core.ILayoutTree {
+            return NewPanelTree();
+        }
+
+        protected measureOverride(constraint: ISize): ISize {
+            return new Size(constraint.width, constraint.height);
+        }
+
+        protected arrangeOverride(arrangeSize: ISize): ISize {
+            return new Size(arrangeSize.width, arrangeSize.height);
+        }
+
+        get childCount(): number {
+            return this.tree.children.length;
+        }
+
+        insertChild(child: core.LayoutNode, index: number) {
+            var children = this.tree.children;
+            if (index >= children.length) {
+                this.appendChild(child);
+            } else if (index <= 0) {
+                this.prependChild(child);
+            } else {
+                children.splice(index, 0, child);
+                child.setParent(this);
+            }
+        }
+
+        prependChild(child: core.LayoutNode) {
+            this.tree.children.unshift(child);
+            child.setParent(this);
+        }
+
+        appendChild(child: core.LayoutNode) {
+            this.tree.children.push(child);
+            child.setParent(this);
+        }
+
+        removeChild(child: core.LayoutNode): boolean {
+            var children = this.tree.children;
+            var index = children.indexOf(child);
+            if (index < 0)
+                return false;
+            this.tree.children.splice(index, 1);
+            child.setParent(null);
+            return true;
+        }
+
+        removeChildAt(index: number): core.LayoutNode {
+            var children = this.tree.children;
+            if (index < 0 || index >= children.length)
+                return null;
+            var removed = children.splice(index, 1)[0];
+            if (removed)
+                removed.setParent(null);
+            return removed;
+        }
+
+        getChildAt(index: number): core.LayoutNode {
+            return this.tree.children[index];
+        }
+    }
+
+    export interface IPanelTree extends core.ILayoutTree {
+        children: core.LayoutNode[];
+    }
+
+    export function NewPanelTree(): IPanelTree {
+        var tree = <IPanelTree>core.DefaultLayoutTree();
+        tree.isLayoutContainer = true;
+        tree.children = [];
+        tree.walk = (reverse?: boolean): core.ILayoutTreeWalker => {
+            if (!reverse) {
+                var i = -1;
+                return {
+                    current: undefined,
+                    step(): boolean {
+                        i++;
+                        if (i >= tree.children.length) {
+                            this.current = undefined;
+                            return false;
+                        }
+                        this.current = tree.children[i];
+                        return true;
+                    },
+                };
+            } else {
+                var i = tree.children.length;
+                return {
+                    current: undefined,
+                    step(): boolean {
+                        i--;
+                        if (i < 0) {
+                            this.current = undefined;
+                            return false;
+                        }
+                        this.current = tree.children[i];
+                        return true;
+                    },
+                }
+            }
+        };
+        return tree;
+    }
+}
---
+++ test/tests/StackPanel.ts
@@ -0,0 +1,77 @@
+namespace mirage.tests {
+    import LayoutFlags = mirage.core.LayoutFlags;
+
+    QUnit.module("StackPanel");
+
+    QUnit.test("draft-vertical", () => {
+        var sp = new StackPanel();
+        sp.inputs.orientation = Orientation.Vertical;
+        sp.invalidateMeasure();
+
+        var child1 = new core.LayoutNode();
+        child1.inputs.width = 100;
+        child1.inputs.height = 50;
+        sp.appendChild(child1);
+
+        var child2 = new core.LayoutNode();
+        child2.inputs.width = 50;
+        child2.inputs.height = 50;
+        child2.inputs.horizontalAlignment = HorizontalAlignment.Center;
+        sp.appendChild(child2);
+
+        var child3 = new core.LayoutNode();
+        child3.inputs.width = 75;
+        child3.inputs.height = 50;
+        child3.inputs.horizontalAlignment = HorizontalAlignment.Right;
+        sp.appendChild(child3);
+
+        // measure
+        ok(sp.measure(new Size(200, Number.POSITIVE_INFINITY)), "measure change");
+        strictEqual(sp.state.flags & LayoutFlags.Measure, 0, "measure flag cleared");
+        deepEqual(sp.state.desiredSize, new Size(100, 150), "desiredSize");
+
+        // arrange
+        ok(sp.arrange(new Rect(0, 0, 200, 200)), "arrange change");
+        strictEqual(sp.state.flags & LayoutFlags.Arrange, 0, "arrange flag cleared");
+        arrangeState(sp, new Rect(0, 0, 200, 200), new Point(), new Size(200, 200), "root");
+        arrangeState(child1, new Rect(0, 0, 200, 50), new Point(50, 0), new Size(100, 50), "child1");
+        arrangeState(child2, new Rect(0, 50, 200, 50), new Point(75, 50), new Size(50, 50), "child2");
+        arrangeState(child3, new Rect(0, 100, 200, 50), new Point(125, 100), new Size(75, 50), "child3");
+    });
+
+    QUnit.test("draft-horizontal", () => {
+        var sp = new StackPanel();
+        sp.inputs.orientation = Orientation.Horizontal;
+        sp.invalidateMeasure();
+
+        var child1 = new core.LayoutNode();
+        child1.inputs.width = 50;
+        child1.inputs.height = 100;
+        sp.appendChild(child1);
+
+        var child2 = new core.LayoutNode();
+        child2.inputs.width = 50;
+        child2.inputs.height = 50;
+        child2.inputs.verticalAlignment = VerticalAlignment.Center;
+        sp.appendChild(child2);
+
+        var child3 = new core.LayoutNode();
+        child3.inputs.width = 50;
+        child3.inputs.height = 75;
+        child3.inputs.verticalAlignment = VerticalAlignment.Bottom;
+        sp.appendChild(child3);
+
+        // measure
+        ok(sp.measure(new Size(Number.POSITIVE_INFINITY, 200)), "measure success");
+        strictEqual(sp.state.flags & LayoutFlags.Measure, 0, "measure flag cleared");
+        deepEqual(sp.state.desiredSize, new Size(150, 100), "desiredSize");
+
+        // arrange
+        ok(sp.arrange(new Rect(0, 0, 200, 200)), "arrange change");
+        strictEqual(sp.state.flags & LayoutFlags.Arrange, 0, "arrange flag cleared");
+        arrangeState(sp, new Rect(0, 0, 200, 200), new Point(), new Size(200, 200), "root");
+        arrangeState(child1, new Rect(0, 0, 50, 200), new Point(0, 50), new Size(50, 100), "child1");
+        arrangeState(child2, new Rect(50, 0, 50, 200), new Point(50, 75), new Size(50, 50), "child2");
+        arrangeState(child3, new Rect(100, 0, 50, 200), new Point(100, 125), new Size(50, 75), "child3");
+    });
+}
