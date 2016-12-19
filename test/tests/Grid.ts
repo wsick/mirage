b2b9b6fc6c28942b6d0af49f288a2dc898f9fd93
Brad Sickles <brad_sickles@mckinsey.com>
2016-12-19 12:00:39 -0500

    Adding star row test.

--- test/tests/Grid.ts
+++ test/tests/Grid.ts
@@ -83,4 +83,42 @@ namespace mirage.tests {
         arrangeState(child2, new Rect(0, 0, 200, 200), new Rect(75, 75, 50, 50), "child2");
         arrangeState(child3, new Rect(0, 0, 200, 200), new Rect(125, 150, 75, 50), "child3");
     });
+
+    QUnit.test("scenario2", () => {
+        var grid = new Grid();
+        // 1* 2* 1* => 25%
+        grid.rowDefinitions = NewRowDefinitions("1* 2* *");
+        // no defined col defs => 1 "auto" col
+        grid.invalidateMeasure();
+
+        var child1 = new core.LayoutNode();
+        child1.width = 100;
+        child1.height = 50;
+        grid.appendChild(child1);
+
+        var child2 = new core.LayoutNode();
+        child2.width = 100;
+        child2.height = 50;
+        Grid.setRow(child2, 1);
+        grid.appendChild(child2);
+
+        var child3 = new core.LayoutNode();
+        child3.width = 100;
+        child3.height = 50;
+        Grid.setRow(child3, 2);
+        grid.appendChild(child3);
+
+        // measure
+        ok(grid.measure(new Size(400, 400)), "measure changed");
+        strictEqual(grid.state.flags & LayoutFlags.measure, 0, "measure flag cleared");
+        deepEqual(grid.state.desiredSize, new Size(100, 150), "desiredSize");
+
+        // arrange
+        ok(grid.arrange(new Rect(0, 0, 400, 400)), "arrange changed");
+        strictEqual(grid.state.flags & LayoutFlags.arrange, 0, "arrange flag cleared");
+        arrangeState(grid, new Rect(0, 0, 400, 400), new Rect(0, 0, 400, 400), "root");
+        arrangeState(child1, new Rect(0, 0, 400, 100), new Rect(150, 25, 100, 50), "child1");
+        arrangeState(child2, new Rect(0, 100, 400, 200), new Rect(150, 175, 100, 50), "child2");
+        arrangeState(child3, new Rect(0, 300, 400, 100), new Rect(150, 325, 100, 50), "child3");
+    });
 }
