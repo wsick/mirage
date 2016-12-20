72fb7c57789ea7a1841c947306399f191faf90d3
Brad Sickles <brad_sickles@mckinsey.com>
2016-12-20 09:59:57 -0500

    Testing Canvas.

---
+++ test/tests/Canvas.ts
@@ -0,0 +1,42 @@
+namespace mirage.tests {
+    import LayoutFlags = mirage.core.LayoutFlags;
+
+    QUnit.module("Canvas");
+
+    QUnit.test("draft-scenario1", () => {
+        var canvas = new Canvas();
+        canvas.invalidateMeasure();
+
+        var child1 = new core.LayoutNode();
+        child1.width = 100;
+        child1.height = 50;
+        canvas.appendChild(child1);
+
+        var child2 = new core.LayoutNode();
+        child2.width = 50;
+        child2.height = 50;
+        Canvas.setLeft(child2, 100);
+        Canvas.setTop(child2, 200);
+        canvas.appendChild(child2);
+
+        var child3 = new core.LayoutNode();
+        child3.width = 75;
+        child3.height = 50;
+        Canvas.setLeft(child3, 300);
+        Canvas.setTop(child3, 600);
+        canvas.appendChild(child3);
+
+        // measure
+        ok(canvas.measure(new Size(600, 600)), "measure changed");
+        strictEqual(canvas.state.flags & LayoutFlags.measure, 0, "measure flag cleared");
+        deepEqual(canvas.state.desiredSize, new Size(0, 0), "desiredSize");
+
+        // arrange
+        ok(canvas.arrange(new Rect(0, 0, 600, 600)), "arrange changed");
+        strictEqual(canvas.state.flags & LayoutFlags.arrange, 0, "arrange flag cleared");
+        arrangeState(canvas, new Rect(0, 0, 600, 600), new Rect(0, 0, 600, 600), "root");
+        arrangeState(child1, new Rect(0, 0, 100, 50), new Rect(0, 0, 100, 50), "child1");
+        arrangeState(child2, new Rect(100, 200, 50, 50), new Rect(100, 200, 50, 50), "child2");
+        arrangeState(child3, new Rect(300, 600, 75, 50), new Rect(300, 600, 75, 50), "child3");
+    });
+}
