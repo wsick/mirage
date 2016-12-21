1cc3f93b7e6b0121db65402f3fd19d52d9b6474f
Brad Sickles <brad_sickles@mckinsey.com>
2016-12-21 11:57:20 -0500

    Finishing testing adapters.

--- test/tests/adapters.ts
+++ test/tests/adapters.ts
@@ -24,5 +24,17 @@ namespace mirage.tests {
         } finally {
             mirage.adapters.unregister(adapter);
         }
+
+        strictEqual(passes.length, 1, "1 slot pass");
+        var firstPass = passes[0];
+        if (firstPass) {
+            strictEqual(firstPass.length, 2, "2 node updates in first slot pass");
+
+            strictEqual(firstPass[0].node, child1, "child1 node update");
+            deepEqual(firstPass[0].newRect, new Rect(0, 0, 100, 50), "child1 new slot");
+
+            strictEqual(firstPass[1].node, canvas, "canvas node update");
+            deepEqual(firstPass[1].newRect, new Rect(0, 0, 600, 600), "canvas new slot");
+        }
     });
 }
